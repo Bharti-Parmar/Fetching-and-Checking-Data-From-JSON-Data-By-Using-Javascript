b06f768a4c85c016a4d19345bdee5289dd372921
Bharti Parmar <38555462+Bharti-Parmar@users.noreply.github.com>
2020-05-15 17:27:05 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,50 @@
+var data = {
+  "mydata" : [
+      {
+        "question":"what is code?",
+        "option":["logic","query","server language","programming"],
+        "answer":"0"
+      },
+      {
+        "question":"what is JS?",
+        "option":["web page","stylesheet","script language","server"],
+        "answer":"2"
+      },
+      {
+        "question":"what is html?",
+        "option":["server","stylesheet","script language","web page"],
+        "answer":"3"
+      },
+      {
+        "question":"what is css?",
+        "option":["style sheet","web page","script language","server"],
+        "answer":"0"
+      }
+    ]
+};
+
+//
+// console.log('data type is:', typeof data);
+// console.log('JSON data type is:', typeof JSON.stringify(data));
+// data.mydata.forEach((item, index) => console.log(`item ${index} in mydata is:`, item))
+
+// var data = {
+//         question:"what is code?",
+//         option:["logic","query","server language","programming"],
+//         answer:"logic"
+//       },
+//       {
+//         question:"what is JS?",
+//         option:["stylesheet","web page","script language","server"],
+//         answer:"script language"
+//       },
+//       {
+//         question:"what is html?",
+//         option:["stylesheet","web page","script language","server"],
+//         answer:"web page"
+//       },
+//       {
+//         question:"what is css?",
+//         option:["style sheet","web page","script language","server"],
+//         answer:"style sheet"
+//   };
